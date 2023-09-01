4ac1a5b5ec418ee1bc6a3f008832d81bd1cf81e2
deepakdashcode <dipudash.2003@gmail.com>
2023-09-01 23:17:04 +0530

    striver dsa started

---
+++ striver/fir.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 5;
+    switch(a)
+    {
+        default: cout << "No Case Matched\n";
+        case 1: cout << 1;
+        break;
+        case 2: cout << 2;
+        break;
+        
+    }
+    return 0;
+}
