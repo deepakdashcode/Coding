ebed9e016e2e0de6c9c25efbddcae4201e578ec1
deepakdashcode <dipudash.2003@gmail.com>
2023-10-10 12:06:39 +0000

    Test

--- owntest/test.cpp
+++ owntest/test.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main(){
      cout << "Hello\n";
      cout<< "Heii\n";
      cout << "Debu";
+     vector<int> v(10);
+     v.push_back(10);
      
      return 0;
 }
